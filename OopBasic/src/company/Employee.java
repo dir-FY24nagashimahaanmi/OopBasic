a0012c54b083d5d7b484825d0d2830e9eb83d36d
dir-FY24nagashimahaanmi <hanami.nagashima.q669@dirbato.co.jp>
2024-04-08 16:59:13 +0900

    Lesson4

--- OopBasic/src/company/Employee.java
+++ OopBasic/src/company/Employee.java
@@ -1,9 +1,9 @@
 package company;
 
-public class Employee {
-	 // フィールド
+public abstract class Employee implements Workable{
+	// フィールド
     protected final String name;
-    private final Department department;
+    protected final Department department;
     private final String position;
     private final int employeeId;
 
@@ -25,9 +25,13 @@ public class Employee {
         report(1);
     }
     
-    //会議に参加するメソッド
-    public void joinMeeting() {
-    	department.meeting();
-    	System.out.println("→上記の会議に参加します。部署：" + department.getName() + "名前：" +name);
+    // 抽象メソッドに変更
+    // 会議に参加するメソッド
+    public abstract void joinMeeting();
+    
+    // 働くメソッドを実装
+    @Override
+    public void work() {
+    	System.out.println("正社員として働きます。名前：" + name + slogan);
     }
 }
